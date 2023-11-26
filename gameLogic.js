36546742587c5f95a540119a92010df582a52b80
ALazyCrow <juanzinacordoba@gmail.com>
2023-11-26 17:49:01 -0500

    Added player score so it can finally keep count, but if you keep playing it'll keep counting, need to be fixed

--- gameLogic.js
+++ gameLogic.js
@@ -1,68 +1,78 @@
-let playerText = document.getElementById("playerText")
-let restartBtn = document.getElementById("restartBtn")
-let boxes = Array.from(document.getElementsByClassName("box"))
-let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks')
-const O_player = "O"
-const X_player = "X"
-let currentPlayer = X_player
-let spaces = Array(9).fill(null)
+let playerText = document.getElementById("playerText");
+let restartBtn = document.getElementById("restartBtn");
+let playerOneScore = document.getElementById("player_x");
+let playerTwoScore = document.getElementById("player_y");
+let boxes = Array.from(document.getElementsByClassName("box"));
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
+const O_player = "O";
+const X_player = "X";
+let currentPlayer = X_player;
+let spaces = Array(9).fill(null);
+let scoreOne = 0;
+let scoreTwo = 0;
 
-
-const startGame = () =>{
-    boxes.forEach(box => box.addEventListener('click',boxclicked))
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxclicked));
 }
 
-function boxclicked(e){
-    const id = e.target.id
+function boxclicked(e) {
+    const id = e.target.id;
 
-    if (!spaces[id]){
-        spaces[id] = currentPlayer
-        e.target.innerText = currentPlayer
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
 
-        if(playerHasWon() !== false){
+        if (playerHasWon() !== false) {
             playerText.innerHTML = `${currentPlayer} has won`;
+            if (currentPlayer == X_player) {
+                scoreOne+=1;
+                playerOneScore.innerHTML = `Player X Score: ${scoreOne}`;
+            }
+            else{
+                scoreTwo+=1;
+                playerTwoScore.innerHTML = `Player Y Score: ${scoreTwo}`;
+            }
+            let winning_blocks = playerHasWon();
 
-            let winning_blocks = playerHasWon()
-
-            winning_blocks.map( box => boxes[box].style.backgroundColor = winnerIndicator)
-            return
+            winning_blocks.map(box => boxes[box].style.backgroundColor = winnerIndicator);
+            return;
         }
 
-        currentPlayer = currentPlayer == X_player ? O_player : X_player
+        currentPlayer = currentPlayer == X_player ? O_player : X_player;
     }
 }
 
 const WinningCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8]
-]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+];
 
-function playerHasWon(){
+function playerHasWon() {
     for (const condition of WinningCombos) {
-        let [a,b,c] = condition
+        let [a, b, c] = condition;
         if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]) {
-            return [a,b,c]
+            return [a, b, c];
         }
     }
-    return false
+    return false;
 }
 
-restartBtn.addEventListener('click', restart)
+restartBtn.addEventListener('click', restart);
 
-function restart(){
-    playerText.innerHTML = `TIC TAC TOE`
-    spaces.fill(null)
-    boxes.forEach( box =>{
-        box.innerText = ""
-        box.style.backgroundColor=""
-    })
-    currentPlayer = X_player
+function restart() {
+    playerText.innerHTML = `TIC TAC TOE`;
+    spaces.fill(null);
+    boxes.forEach(box => {
+        box.innerText = "";
+        box.style.backgroundColor = "";
+    });
+    currentPlayer = X_player;
 }
 
-startGame()
+startGame();
